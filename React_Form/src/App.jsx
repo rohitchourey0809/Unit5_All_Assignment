1035c3760fde088a41d82394fbbe564df4b771ad
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-04-17 19:57:07 +0530

    ok

---
+++ React_Form/src/App.jsx
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import "./App.css";
+// import { useState } from "react"
+import { FORM } from "./components/form";
+function App() {
+  // const [counter,setcounter] = useState(50)
+
+  return (
+    <div className="App">
+      {/* <button>Button</button> */}
+      <FORM />
+    </div>
+  );
+}
+
+export default App;
