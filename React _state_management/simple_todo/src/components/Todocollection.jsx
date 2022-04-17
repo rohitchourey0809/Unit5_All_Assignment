390456ee3c29072bd4dbbc3ce7031bbae064c527
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-04-18 01:25:39 +0530

    f

--- React _state_management/simple_todo/src/components/Todocollection.jsx
+++
@@ -1,9 +0,0 @@
-export const Todocollection = ({ todo, handlestatus }) => {
-    return (
-        <div>
-            {/* <div>id:{todo.id}</div> */}
-            {todo.title} -{todo.status ? "Done" : "Not Done"}{" "}
-            <button onClick={() => handlestatus(todo.id)}>Toggle</button>
-        </div>
-    );
-};
