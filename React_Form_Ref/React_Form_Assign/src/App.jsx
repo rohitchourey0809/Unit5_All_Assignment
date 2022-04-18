36c18f76a58cb10b31f431e9555a7fa0357d8232
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-04-18 22:05:14 +0530

    master

--- React_Form_Ref/React_Form_Assign/src/App.jsx
+++ React_Form_Ref/React_Form_Assign/src/App.jsx
@@ -2,13 +2,19 @@
 import "./App.css";
 // import { useState } from "react"
 import { FORM } from "./components/form";
+import { ShowData } from "./components/showdata"
 function App() {
   // const [counter,setcounter] = useState(50)
 
   return (
     <div className="App">
       {/* <button>Button</button> */}
+
       <FORM />
+
+      <div className="showdata">
+        <ShowData />
+      </div>
     </div>
   );
 }
