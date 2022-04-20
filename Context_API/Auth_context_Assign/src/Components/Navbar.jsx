bd63a7379d36238742bba96e6aacaeae6026c3cb
ROHIT CHOUREY <97465195+rohitchourey0809@users.noreply.github.com>
2022-04-21 02:33:41 +0530

    ok

---
+++ Context_API/Auth_context_Assign/src/Components/Navbar.jsx
@@ -0,0 +1,8 @@
+export const NAVBAR = () => {
+  return (
+    <div className="navdiv">
+      <button className="login">Login</button>
+      <button className="logout">Logout</button>
+    </div>
+  );
+};
---
+++ Context_API/Auth_context_Assign/src/Context/authcontest.jsx
@@ -0,0 +1,18 @@
+//?Step1 init createContext c
+import {useContext} from "react"
+
+//take a Empty bag
+
+// export const AuthContext = useContext()
+
+// //?Step2 rendering part
+
+// const AuthContextProvider = ({children}) =>{
+
+
+//     return (
+// < AuthContext.Provider>
+// {children}
+// </AuthContext.Provider>
+//     )
+// }
